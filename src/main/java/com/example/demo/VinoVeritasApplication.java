63adc22b9e201ca69e98481faed3681f13c795d0
Maria <mariellariccardi06@gmail.com>
2019-10-22 12:06:58 +0200

    first commit

---
+++ src/main/java/com/example/demo/VinoVeritasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinoVeritasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinoVeritasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/VinoVeritasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinoVeritasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
